987f3bce41d2c08a9ef9199305aaf2f2a6d89f78
ZokaKami <ruffy92@live.com>
2025-01-29 14:41:17 +0100

    Tailwind 4.0 installation test

---
+++ components/test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Test() {
+  return (
+    <div>
+      <h3 className="text-[64px]">hey</h3>
+    </div>
+  );
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Test from "../components/test.tsx";
+function App() {
+  return (
+    <>
+      <Test />
+      <h1 className="text-[64px]">hi</h1>
+    </>
+  );
+}
+
+export default App;
